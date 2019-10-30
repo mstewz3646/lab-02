2a22656d86c7e32eac616557fed0393a9909b5c7
Mark Swearingen <markswearingen@bendcable.com>
2019-10-30 16:02:20 -0700

    Replace Json file for new links

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 'use strict';
 
+// --------Global Variables-----------------
+let keywords = [];
+
 function Horn(horn){
   this.image_url = horn.image_url;
   this.title = horn.title;
@@ -10,9 +13,7 @@ function Horn(horn){
 
 Horn.allHorns = [];
 
-Horn.prototype.render = function(){
-  console.log ('render');
-  
+Horn.prototype.render = function(){ 
   //do something
   //1. create element
   let hornClone = $('#photo-template').clone();
@@ -30,23 +31,23 @@ Horn.prototype.render = function(){
 };
 
 
-Horn.prototype.filter = function(){
-  console.log ('filter');
-  
+Horn.prototype.filter = function(){ 
   //do something
   //1. create element
   let hornClone = $('option').clone();
- console.log(hornClone);
  let $hornClone = $(hornClone[0]);
- console.log($hornClone);
   //2. give it content
   // <option value="narwhal">Narwal</option>
-   console.log($hornClone);
   $hornClone.attr('value', this.keyword);
   $hornClone.text(this.keyword);
 
   //3. append to DOM
-  $hornClone.appendTo('#animal-select');
+  
+  // Add if statement in case keyword is already in keywords array
+  if (!keywords.includes(this.keyword)){
+    $hornClone.appendTo('#animal-select');
+    keywords.push(this.keyword);
+  }
 };
 
 Horn.readJson = () => {
@@ -54,23 +55,17 @@ Horn.readJson = () => {
   .then(page => {
     page.forEach(item => {
       Horn.allHorns.push(new Horn(item));
-      // console.log (item);
     });
   })
   .then(Horn.loadHorns);
-  // console.log ('hello');
 };
 
 Horn.loadHorns = () => {
-  console.log ('bye');
   Horn.allHorns.forEach(horn => {
     horn.render();
     horn.filter();
-    // console.log (horn);
   });
 };
-
-console.log(Horn.allHorns);
 
 $(() => Horn.readJson());
 
